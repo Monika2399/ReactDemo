f0a15886e4c46d246b6a386d27ec3740e0a2f523
Monika2399 <moni.selva99@gmail.com>
2024-05-12 12:48:06 +0530

    Changed text in App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function App() {
   return (
-   <div style={{display:"flex",justifyContent:"center",fontSize:"60px"}}>REACT</div>
+   <div style={{display:"flex",justifyContent:"center",fontSize:"60px"}}>REACT DEMO APP</div>
   );
 }
 
